88f48f9463270d5ab5849cecaa19655560dd798f
sasha370 <kng.sasha@mail.ru>
2020-09-10 18:40:05 +0300

    add first tests for Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,  :trackable
+
+  validates :last_name, :first_name, presence: true
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'creation' do
+
+    before do
+      # блок будет выполнен перед каждым тестом в этом разделе
+      @user = User.create(email: 'test@test.ru', password: '123456', password_confirmation: '123456', first_name: 'John', last_name: 'Connor')
+    end
+
+      # User может быть создан
+    it "can be created" do
+      expect(@user).to be_valid
+    end
+
+    # User не может быть создан без этих полей
+    it 'cannot be created without first_name and last_name' do
+      # удаляем заданные строки
+      @user.first_name = nil
+      @user.last_name = nil
+      expect(@user).to_not be_valid
+    end
+
+
+  end
 end
