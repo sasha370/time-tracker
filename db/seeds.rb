16a40a8c443d34cac0eb679023b5745f15acfde1
sasha370 <kng.sasha@mail.ru>
2020-10-01 14:26:16 +0300

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content")
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user: User.last)
 end
 
 puts " 100 Posts was created"
